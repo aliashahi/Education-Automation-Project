e6f19b5988a821a7ad2a93748697939eb7fee362
Ali Akbar Shahi <aliakbarshahice@gmail.com>
2021-10-17 10:08:56 +0330

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'EAP-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Education-Automation-Project';
+}
