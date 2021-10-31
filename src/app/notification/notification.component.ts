8e7e25a99ce45485ed24293985e5d08314a7c287
HasanFaaour <1abo1995ali@gmail.com>
2021-10-31 08:28:33 +0330

    modify

---
+++ src/app/notification/notification.component.ts
@@ -0,0 +1,33 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-notification',
+    templateUrl: './notification.component.html',
+    styleUrls: ['./notification.component.css']
+})
+export class notificationComponent {
+    title : string = '';
+    explanation : string = '';
+    notific : string = '';
+    mesage1 : string = "The notification field can't be empty!... Please enter something.";
+    mesage2 : string = "Your notification has been saved";
+    mesage_active = false;
+    submited(){
+        if (this.notific.localeCompare('')==0){
+            this.mesage2 = '';
+            this.mesage1 = "The notification field can't be empty!... Please enter something.";
+            this.mesage_active = true; 
+        }
+        else{
+            this.mesage1 = '';
+            this.mesage2 = "Your notification has been saved";
+            this.mesage_active = true;
+            this.title = '';
+            this.explanation = ''; 
+            this.notific = '';
+        }
+        setTimeout(() => {
+            this.mesage_active = false;
+        }, 10000);
+    }
+}
