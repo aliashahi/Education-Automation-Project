633521107080243bbf6d48da322d03cb3d64d567
Ali Akbar Shahi <aliakbarshahice@gmail.com>
2021-10-31 12:51:58 +0330

    ADD login form controllers

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
@@ -8,12 +9,32 @@ import { AuthService } from '../../services/auth.service';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  forms = new FormGroup({
+    username: new FormControl(null, [Validators.required]),
+    password: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    role: new FormControl('S'),
+    remember_me: new FormControl(false),
+  });
+  pendding = false;
+
   constructor(private router: Router, private authService: AuthService) {}
 
   ngOnInit(): void {}
 
   onLogin() {
-    this.router.navigate(['/']);
+    if (this.forms.valid) {
+      this.pendding = true;
+      this.forms.disable();
+      setTimeout(() => {
+        this.pendding = false;
+        this.forms.enable();
+        localStorage.setItem('Token', 'MY_JWT_TOKEN');
+        this.router.navigate(['/']);
+      }, 3000);
+    }
   }
 
   onRedirectToRegister() {
--- src/app/core/gaurd/auth.guard.ts
+++ src/app/core/gaurd/auth.guard.ts
@@ -11,7 +11,7 @@ export class AuthGuard implements CanActivate {
     if (localStorage.getItem('Token')) {
       return true;
     }
-    this.router.navigate(['/auth']);
+    this.router.navigate(['/auth/login']);
     return false;
   }
 }
