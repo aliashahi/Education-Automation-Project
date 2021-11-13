01125b18528a9e9c09692e8d7458ff5daeaba49b
MSNP1381 <mohamadnematpoor@gmail.com>
2021-11-13 17:28:57 +0330

    teacher class

---
+++ src/app/teacher/components/teacher-class/teacher-class.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'EAP-teacher-class',
+  templateUrl: './teacher-class.component.html',
+  styleUrls: ['./teacher-class.component.scss']
+})
+export class TeacherClassComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/teacher/teacher.module.ts
+++ src/app/teacher/teacher.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
-import { SharedModule } from '../shared/shared.module';
-import { TeacherComponent } from './container/teacher/teacher.component';
-import { TeacherDashboardComponent } from './components/teacher-dashboard/teacher-dashboard.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {SharedModule} from '../shared/shared.module';
+import {TeacherComponent} from './container/teacher/teacher.component';
+import {TeacherDashboardComponent} from './components/teacher-dashboard/teacher-dashboard.component';
 import {MatDatepickerModule} from '@angular/material/datepicker';
-import { TeacherClassComponent } from './components/teacher-class/teacher-class.component';
+import {TeacherClassComponent} from './components/teacher-class/teacher-class.component';
+import {TeacherClassesComponent} from './components/teacher-classes/teacher-classes.component';
+
 const routes: Routes = [
   {
     path: '',
@@ -20,6 +22,10 @@ const routes: Routes = [
         component: TeacherClassComponent
       },
       {
+        path: 'classes',
+        component: TeacherClassesComponent
+      },
+      {
         path: '**',
         redirectTo: 'dashboard',
       },
@@ -32,8 +38,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [TeacherComponent, TeacherDashboardComponent, TeacherClassComponent],
-  imports: [CommonModule, SharedModule, RouterModule.forChild(routes),MatDatepickerModule],
+  declarations: [TeacherComponent, TeacherDashboardComponent, TeacherClassComponent, TeacherClassesComponent],
+  imports: [CommonModule, SharedModule, RouterModule.forChild(routes), MatDatepickerModule],
 
 })
-export class TeacherModule {}
+export class TeacherModule {
+}
