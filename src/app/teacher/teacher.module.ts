a0ce53bb7177af069d49aa21bf236ce8e4bf87e3
HasanFaaour <1abo1995ali@gmail.com>
2021-11-15 22:04:17 +0330

    Merge branch 'dev' of https://github.com/aliashahi/Education-Automation-Project into dev

--- src/app/teacher/teacher.module.ts
+++ src/app/teacher/teacher.module.ts
@@ -4,14 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { TeacherComponent } from './container/teacher/teacher.component';
 import { TeacherDashboardComponent } from './components/teacher-dashboard/teacher-dashboard.component';
-<<<<<<< HEAD
 import { AnnouncementComponent } from './components/announcement/announcement.component';
 import {FormsModule} from '@angular/forms';
 
-=======
 import {MatDatepickerModule} from '@angular/material/datepicker';
-import { TeacherClassComponent } from './components/teacher-class/teacher-class.component';
->>>>>>> b227399c68dd15d1692a25b5dc4d8b579e4803ef
+//import { TeacherClassComponent } from './components/teacher-class/teacher-class.component';
+
 const routes: Routes = [
   {
     path: '',
@@ -22,13 +20,8 @@ const routes: Routes = [
         component: TeacherDashboardComponent,
       },
       {
-<<<<<<< HEAD
         path: 'announcement',
         component: AnnouncementComponent,
-=======
-        path: 'class',
-        component: TeacherClassComponent
->>>>>>> b227399c68dd15d1692a25b5dc4d8b579e4803ef
       },
       {
         path: '**',
@@ -43,14 +36,8 @@ const routes: Routes = [
 ];
 
 @NgModule({
-<<<<<<< HEAD
   declarations: [TeacherComponent, TeacherDashboardComponent, AnnouncementComponent],
-  imports: [CommonModule, SharedModule,FormsModule, RouterModule.forChild(routes)],
-=======
-  declarations: [TeacherComponent, TeacherDashboardComponent, TeacherClassComponent],
-  imports: [CommonModule, SharedModule, RouterModule.forChild(routes),MatDatepickerModule],
-
->>>>>>> b227399c68dd15d1692a25b5dc4d8b579e4803ef
+  imports: [CommonModule, SharedModule,FormsModule, RouterModule.forChild(routes),MatDatepickerModule],
 })
 export class TeacherModule {}
 
