72b5f4909dbf83612e8e7bb0aff7a725ea32fb6e
Ali Akbar Shahi <aliakbarshahice@gmail.com>
2021-11-19 22:53:16 +0330

    FIX add toaster in logout

--- src/app/main/components/topnav/topnav.component.ts
+++ src/app/main/components/topnav/topnav.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/auth/services/user.service';
+import { AlertService } from 'src/app/shared/modules/alert/alert.service';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'EAP-topnav',
@@ -10,7 +11,11 @@ import { UserService } from 'src/app/auth/services/user.service';
 export class TopnavComponent implements OnInit {
   @Input() isExpanded: boolean = false;
   @Output() isExpandedChange = new EventEmitter<boolean>();
-  constructor(private router: Router, private userSrv: UserService) {}
+  constructor(
+    private router: Router,
+    private userSrv: UserService,
+    private alertSrv: AlertService
+  ) {}
 
   ngOnInit(): void {
     this.userSrv.getMyInfo().subscribe((res) => {
@@ -20,6 +25,7 @@ export class TopnavComponent implements OnInit {
 
   onLogout() {
     localStorage.clear();
+    this.alertSrv.showToaster('You loged out Successfully!', 'INFO');
     this.router.navigate(['/auth/login']);
   }
 }
