e159327ce23f595f99ab0219a2a502b1e69bd4a5
Ali Akbar Shahi <aliakbarshahice@gmail.com>
2021-12-05 12:17:29 +0330

    FIX profile extra info

--- src/app/shared/utils/date.utils.ts
+++ src/app/shared/utils/date.utils.ts
@@ -3,6 +3,6 @@ export function createDateFormat(date: Date): string {
     let d = date.toLocaleDateString('en-US').split('/');
     return d[2] + '-' + d[0] + '-' + d[1];
   } catch {
-    return '';
+    return createDateFormat(new Date());
   }
 }
